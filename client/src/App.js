947ae60dde6bdbd77f5586419a3fa4b4613e059b
Ankur Saha <ankursahamoto@gmail.com>
2021-06-05 21:43:46 +0530

    browser scrolling fixed

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,19 @@ const App = () => {
       .get(`${backendUrl}/api/auth/profile`)
       .then((res) => setUser(res.data.data.user))
       .catch((err) => setUser(null));
+
+    resizeHeightOnMobile();
   }, []);
+  const resizeHeightOnMobile = () => {
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    window.addEventListener('resize', () => {
+      // We execute the same script as before
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+  };
   return (
     <div className="App no-select">
       <Switch>
